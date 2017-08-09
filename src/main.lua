088036274aefc75c2e970240f44ee7815868ef12
Paul Liverman III <paul.liverman.iii@gmail.com>
2017-08-09 16:00:54 -0700

    consoles global (yay APIs)

--- src/main.lua
+++ src/main.lua
@@ -1,4 +1,4 @@
-local consoles = {}
+os.loadAPI("src/apis/consoles")
 
 local selector = dofile("src/selector.lua")
 selector.init(consoles) -- console selector needs to have access to consoles
@@ -7,8 +7,18 @@ local function new_console(side)
   consoles[side] = selector.console(peripheral.wrap(side))
 end
 
+-- restore previous arrangement
+if fs.exists(".consoles") then
+  local file = fs.open(".consoles", "r")
+  local restore = textutils.unserialize(file.readAll())
+  file.close()
+  for side, name in pairs(restore) do
+    consoles[side] = dofile("src/consoles/" .. name .. ".lua")(peripheral.wrap(side))
+  end
+end
+
 for _, side in ipairs(peripheral.getNames()) do
-  if peripheral.getType(side) == "monitor" then
+  if (not consoles[side]) and peripheral.getType(side) == "monitor" then
     new_console(side)
   end
 end
